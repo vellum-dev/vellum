79b207e80c64c97032b12d4ba51ca47cffeed618
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2026-07-08 16:27:24 -0600

    Review fix, change categories

--- packages/appload/launcherctl.prestart.sh
+++ packages/appload/launcherctl.prestart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cat << EOF > "/etc/systemd/system/xochitl.service.d/launcherctl-failure.conf"
+cat <<EOF >"/etc/systemd/system/xochitl.service.d/launcherctl-failure.conf"
 [Unit]
+OnFailure=
 OnFailure=launcherctl-failure.service
 EOF
 systemctl daemon-reload
